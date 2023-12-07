30297da5cea5eaca642b6616396fdf35f379a0fd
ramanandh25 <ramanandh.sb@gmail.com>
2023-12-07 13:42:55 +0530

    Intialized dvc and modified the dockerfile

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,11 +1,16 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
+from cybulde.utils.utils import get_logger
+from cybulde.utils.data_utils import initialize_dvc
+from pathlib import Path
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    # logger = get_logger(Path(__file__).name)
+    initialize_dvc()
+    # print(config)
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()  # type: ignore
