6102a851ba8a45c9d6834683d3fdab75e9f9c9ad
ramanand <ramanandh.sb@gmail.com>
2023-12-07 15:48:29 +0100

    Updated version of the data from v1 to v2

--- cybulde/config_schemas/config_schema.py
+++ cybulde/config_schemas/config_schema.py
@@ -5,7 +5,7 @@ from pydantic.dataclasses import dataclass
 @dataclass
 class Config:
     dvc_remote_name: str = "gcs-storage"
-    dvc_remote_url: str = "gs://ram_mlops_bucket/data/raw"
+    dvc_remote_url: str = "gs://ram_mlops_udemy/data/raw"
     dvc_raw_data_folder: str = "data/raw"
 
 
